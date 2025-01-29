24a6a36dbd909010d1c3e8e8208cf141558da926
ItemCloud4 <itemcloud4@gmail.com>
2025-01-29 16:49:51 +0200

    Delete assets directory

--- assets/index-B7EHgSrN.js
+++
@@ -1,1 +0,0 @@
-import{j as r,s}from"./index-DMySAKZv.js";const p=({children:a})=>r.jsx("h2",{className:s.paragraf5_b,children:a}),e=({children:a})=>r.jsx("h2",{className:s.paragraf5_g,children:a});export{e as P,p as a};
